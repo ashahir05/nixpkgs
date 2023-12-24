8742edf250d2c5710a4b8e1e46bee860292b49c4
Ahmed Shahir Samin <ashahir05@gmail.com>
2023-12-24 23:05:19 +0600

    Fix outputs

--- flake.nix
+++ flake.nix
@@ -14,31 +14,22 @@
       ];
       recurse = val: pkgs: if builtins.typeOf(val) == "set" then (nixpkgs.lib.mapAttrs (k: v: if v ? type && v.type == "derivation" then (wrapPackage v pkgs) else (recurse v pkgs)) val) else val;
       wrapPackage = pkg: pkgs: (
-        if pkg ? type && pkg.type == "derivation" then pkgs.stdenv.mkDerivation ({
+        if pkg ? type && pkg.type == "derivation" then pkgs.stdenv.mkDerivation (rec {
           inherit (pkg) name outputs passthru meta;
+          src = pkg;
+          buildInputs= [ pkg pkgs.mesa.drivers ];
           nativeBuildInputs= [ pkg pkgs.mesa.drivers ];
-          buildCommand = ''
-            set -euo pipefail
-            ${
-              nixpkgs.lib.concatStringsSep "\n"
-                (map
-                  (outputName:
-                    ''
-                      mkdir -p $out/bin
-                      for bin in ${pkg.${outputName}}/bin/*; do
-                        bin_name=$(basename $bin)
-                        echo "#!/bin/sh" > $out/bin/$bin_name
-                        echo "export LD_LIBRARY_PATH=${pkgs.mesa.drivers}/lib" >> $out/bin/$bin_name
-                        echo "export LIBGL_DRIVERS_PATH=${pkgs.mesa.drivers}/lib/dri" >> $out/bin/$bin_name
-                        echo "exec $bin \"\$@\"" >> $out/bin/$bin_name
-                        chmod +x $out/bin/$bin_name
-                      done
-                    ''
-                  )
-                  (pkg.outputs or ["out"])
-                )
-            }
-          '';
+          installPhase = builtins.concatStringsSep ''''\n'' (builtins.map (output: let oldOut = pkg."${output}"; out = output; in ''
+            mkdir -p ''$${out}/bin
+            for orgBin in ${oldOut}/bin/*; do
+              bin_name=$(basename $orgBin)
+              echo "#!/bin/sh" > ''$${out}/bin/$bin_name
+              echo "export LD_LIBRARY_PATH=${pkgs.mesa.drivers}/lib" >> ''$${out}/bin/$bin_name
+              echo "export LIBGL_DRIVERS_PATH=${pkgs.mesa.drivers}/lib/dri" >> ''$${out}/bin/$bin_name
+              echo "exec ${oldOut}/bin/$bin_name "''$\@"" >> ''$${out}/bin/$bin_name
+              chmod +x ''$${out}/bin/$bin_name
+            done
+          '') outputs);
         }) else pkg
       );
     in
