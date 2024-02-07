5fd48cc7683d6ae2911338af7fbbbf13604cb6fc
Ahmed Shahir <44367115+ashahir05@users.noreply.github.com>
2024-02-07 15:04:36 +0600

    Update wrap.sh

--- wrap.sh
+++ wrap.sh
@@ -16,8 +16,8 @@ for output_name in $2; do
     bin_name=$(basename $orgBin)
     mv $orgBin $out/bin_org/$bin_name
     echo "#!/bin/sh" > $out/bin/$bin_name
-    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$1/lib" >> $out/bin/$bin_name
-    echo "export LIBGL_DRIVERS_PATH=$LD_LIBRARY_PATH:$1/lib/dri" >> $out/bin/$bin_name
+    echo "export LD_LIBRARY_PATH=$1/lib" >> $out/bin/$bin_name
+    echo "export LIBGL_DRIVERS_PATH=$1/lib/dri" >> $out/bin/$bin_name
     echo "exec $out/bin_org/$bin_name \"\$@\"" >> $out/bin/$bin_name
     chmod +x $out/bin/$bin_name
   done
