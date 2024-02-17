836ff15261d57201ef410930bdf7133f01257615
Ahmed Shahir <ashahir05@gmail.com>
2024-02-17 22:25:52 +0600

    Fix symlinks

--- pkg.nix
+++ pkg.nix
@@ -16,11 +16,12 @@
           
           cd ${orgPkg."${x}"}
           find -L * -type d -exec mkdir -p $out/{} \;
-          find -L * -type f -exec touch $out/{} \;
+          find -L * -type f -exec ln -s ${orgPkg."${x}"}/{} $out/{} \;
 
           for orgBin in $out/bin/*; do
             [ -e "$orgBin" ] || continue
             bin_name=$(basename $orgBin)
+            rm $out/bin/$bin_name
             echo "#!/bin/sh" > $out/bin/$bin_name
             echo "export LD_LIBRARY_PATH=${pkgs.mesa.drivers}/lib" >> $out/bin/$bin_name
             echo "export LIBGL_DRIVERS_PATH=${pkgs.mesa.drivers}/lib/dri" >> $out/bin/$bin_name
