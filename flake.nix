a962adc195cca4e5754fab5578dda6bfddfa79fa
Ahmed Shahir <ashahir05@gmail.com>
2024-02-17 22:11:02 +0600

    Rewrite

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
         "aarch64"
       ];
       recurse = val: pkgs: if builtins.typeOf(val) == "set" then (nixpkgs.lib.mapAttrs (k: v: if v ? type && v.type == "derivation" then (wrapPackage v pkgs) else (recurse v pkgs)) val) else val;
-      wrapPackage = pkg: pkgs: pkgs.callPackage ./pkg.nix { inherit pkg pkgs; }
+      wrapPackage = pkg: pkgs: (pkgs.callPackage ./pkg.nix { inherit pkg pkgs; });
     in
     {
       inherit (nixpkgs) lib;
--- pkg.nix
+++ pkg.nix
@@ -1,7 +1,35 @@
-{ pkg, pkgs, ... }: {
-  stdenv.mkDerivation {
-    installPhase = ''
-      mkdir $out
-    '';
-  }
-}
+{ pkg, pkgs, ... } @ args:
+  let 
+    finalPkg = pkg.override (builtins.removeAttrs args [ "pkg" "pkgs" ]);
+    build = orgPkg: ({
+      name = orgPkg.name;
+      outputs = orgPkg.outputs;
+      targetDrv = orgPkg;
+      builder = "${pkgs.bash}/bin/bash";
+      args = [
+        "-c"
+        (pkgs.lib.concatStringsSep "\n" (builtins.concatMap (x: [''
+          export PATH="${pkgs.coreutils}/bin:${pkgs.findutils}/bin"
+          outname="${x}"
+          out=''${!outname}
+          mkdir -p $out
+          
+          cd ${orgPkg."${x}"}
+          find -L * -type d -exec mkdir -p $out/{} \;
+          find -L * -type f -exec touch $out/{} \;
+
+          for orgBin in $out/bin/*; do
+            [ -e "$orgBin" ] || continue
+            bin_name=$(basename $orgBin)
+            echo "#!/bin/sh" > $out/bin/$bin_name
+            echo "export LD_LIBRARY_PATH=${pkgs.mesa.drivers}/lib" >> $out/bin/$bin_name
+            echo "export LIBGL_DRIVERS_PATH=${pkgs.mesa.drivers}/lib/dri" >> $out/bin/$bin_name
+            echo "export VK_DRIVER_FILES=${pkgs.mesa.drivers}/share/vulkan/icd.d" >> $out/bin/$bin_name
+            echo "exec ${orgPkg."${x}"}/bin/$bin_name \"\$@\"" >> $out/bin/$bin_name
+            chmod +x $out/bin/$bin_name
+          done
+        '']) orgPkg.outputs))
+      ];
+    });
+  in
+    (pkgs.stdenv.mkDerivation (build finalPkg)) // { overrideAttrs = x: pkgs.stdenv.mkDerivation (build (finalPkg.overrideAttrs x));  }
