38142132e743b8f6a4be24a7b6b2a04b1335efee
Ahmed Shahir Samin <ashahir05@gmail.com>
2024-05-22 00:56:57 +0600

    Clean up

--- flake.nix
+++ flake.nix
@@ -1,25 +1,24 @@
 {
-  description = "Nix GL Fixes";
+  description = "nixpkgs+unfree+gl";
 
   inputs = {  
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
-      inherit (nixpkgs) lib nixosModules htmlDocs;
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64"
-      ];
+      systems = lib.systems.flakeExposed;
+      lib = nixpkgs.lib;
+      eachSystem = lib.genAttrs systems;
       recurse = val: pkgs: if builtins.typeOf(val) == "set" then (nixpkgs.lib.mapAttrs (k: v: if v ? type && v.type == "derivation" then (wrapPackage v pkgs) else (recurse v pkgs)) val) else val;
       wrapPackage = pkg: pkgs: (pkgs.callPackage ./pkg.nix { inherit pkg pkgs; });
     in
     {
-      inherit (nixpkgs) lib;
-      legacyPackages = forAllSystems (system:
-        let pkgs = import nixpkgs { config.allowUnfree = true; inherit system; };
+      inherit (nixpkgs) lib nixosModules htmlDocs;
+      legacyPackages = eachSystem (system:
+        let pkgs = import nixpkgs { config.allowUnfree = true; config.allowUnsupportedSystem = true; inherit system; };
         in recurse pkgs pkgs
       );
+      checks = eachSystem (system: import ./checks.nix { nixpkgs = self.legacyPackages.${system}; });
     };
 }
