2b3c298e8666e8680fe8058e140e1d73a42dc620
Ahmed Shahir Samin <ashahir05@gmail.com>
2024-06-30 21:34:03 +0600

    Tidy up

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "nixpkgs+unfree+gl";
+  description = "nixpkgs+unfree";
 
   inputs = {  
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -10,14 +10,11 @@
       systems = lib.systems.flakeExposed;
       lib = nixpkgs.lib;
       eachSystem = lib.genAttrs systems;
-      recurse = val: pkgs: if builtins.typeOf(val) == "set" then (nixpkgs.lib.mapAttrs (k: v: if v ? type && v.type == "derivation" then (wrapPackage v pkgs) else (recurse v pkgs)) val) else val;
-      wrapPackage = pkg: pkgs: (pkgs.callPackage ./pkg.nix { inherit pkg pkgs; });
     in
     {
       inherit (nixpkgs) lib nixosModules htmlDocs;
       legacyPackages = eachSystem (system:
-        let pkgs = import nixpkgs { config.allowUnfree = true; config.allowUnsupportedSystem = true; inherit system; };
-        in recurse pkgs pkgs
+        import nixpkgs { config.allowUnfree = true; config.allowUnsupportedSystem = true; inherit system; }
       );
     };
 }
