1432f553d39e3750ef995ba4e4f333163be9d7c1
Ahmed Shahir <ashahir05@gmail.com>
2024-08-19 05:19:20 +0600

    Fixes

--- wrap.nix
+++ wrap.nix
@@ -1,6 +1,6 @@
 { nixpkgs }: (pkg:
   nixpkgs.stdenv.mkDerivation {
-    inherit (pkg) name version meta passthru;
+    inherit (pkg) name version meta outputs passthru;
     buildInputs = [ pkg ];
     nativeBuildInputs = [ pkg ];
     buildCommand = ''
@@ -33,7 +33,7 @@
                 set +x
               ''
             )
-            (if pkg ? outputs then pkg.outputs else [ "out" ])
+            (pkg.outputs)
           )
       }
     '';
